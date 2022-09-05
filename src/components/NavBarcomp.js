864e8becde0c5f0c0b283e1810abc57c16545218
geronimocartasega <gerocartasegna10@hotmail.com>
2022-09-05 12:13:35 -0300

    navbar

--- src/components/NavBarcomp.js
+++ src/components/NavBarcomp.js
@@ -3,19 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 export default class NavBarcomp extends Component {
     render() {
         return(
             <div>
               <Navbar bg="dark" variant={"dark"} expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">HypeClub</Navbar.Brand>
+                    <Navbar.Brand className="brand" href="#inicio">Hype<span className="brand-span">Club</span></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Inicio</Nav.Link>
-                        <Nav.Link href="#link">Productos</Nav.Link>
-                        <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
+                    <Nav className="d-flex justify-content-center">
+                        <Nav.Link href="#inicio">Inicio</Nav.Link>
+                        <Nav.Link href="#Productos">Productos</Nav.Link>
+                        <Nav.Link href="#Link">Sobre Nosotros</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
